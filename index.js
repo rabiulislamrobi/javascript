66bc1cf25d22798f494f423d715b011d53dc98b2
Rabiul Miah <147720141+rabiulislamrobi@users.noreply.github.com>
2023-12-05 12:19:57 +0600

    Add files via upload
    javascript output

---
+++ index.js
@@ -0,0 +1,20 @@
+// write function 
+document.write("Name:rabiul hassan robi<br>");
+document.write("Father's name:abul hossain<br>");
+document.write("Mother's name: rohima begum<br>");
+document.write("location:pubergaon,atlapur ,rupganj,narayanganj,dhaka ,bangladesh<br>")
+//  log functon 
+console.log("University:comilla university");
+console.log("dept:computer science and engineering ")
+console.log("result:first class");
+
+// alert functon 
+window.alert("rabiul hassan robi");
+window.alert("bangldesh");
+
+
+
+
+
+
+
